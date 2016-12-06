0ad5186ff81daeb915c753fa028dcd480d470705
Elena Willen <lnamaso@hotmail.com>
2016-12-06 16:09:07 +0000

    submit challenge

--- Destini/ViewController.swift
+++ Destini/ViewController.swift
@@ -33,6 +33,8 @@ class ViewController: UIViewController {
     
     // TODO Step 5: Initialise instance variables here
     
+    var storyIndex = 1
+    
     
     
     
@@ -42,6 +44,8 @@ class ViewController: UIViewController {
         
         // TODO Step 3: Set the text for the storyTextView, topButton, bottomButton, and to T1_Story, T1_Ans1, and T1_Ans2
         
+        startStory()
+        
     }
 
     
@@ -49,12 +53,71 @@ class ViewController: UIViewController {
     @IBAction func buttonPressed(_ sender: UIButton) {
     
         // TODO Step 4: Write an IF-Statement to update the views
+        
+        if storyIndex == 1 && sender.tag == 1 {
+            
+            storyTextView.text = story2
+            topButton.setTitle(answer2a, for: .normal)
+            bottomButton.setTitle(answer2b, for: .normal)
+            storyIndex = 2
+            
+        } else if storyIndex == 1 && sender.tag == 2 {
+            
+            storyTextView.text = story3
+            topButton.setTitle(answer3a, for: .normal)
+            bottomButton.setTitle(answer3b, for: .normal)
+            storyIndex = 3
+
+            
+        } else if storyIndex == 2 && sender.tag == 1 {
+                
+            storyTextView.text = story3
+            topButton.setTitle(answer3a, for: .normal)
+            bottomButton.setTitle(answer3b, for: .normal)
+            storyIndex = 3
+                
+        } else if storyIndex == 2 && sender.tag == 2 {
+    
+            storyTextView.text = story4
+            topButton.setTitle("End of Story! Start at the beginning!", for: .normal)
+            bottomButton.isHidden = true
+            storyIndex = 4
+
+
+        } else if storyIndex == 3 && sender.tag == 1 {
+                
+            storyTextView.text = story6
+            topButton.setTitle("End of Story! Start at the beginning!", for: .normal)
+            bottomButton.isHidden = true
+            storyIndex = 6
+
+
+        } else if storyIndex == 3 && sender.tag == 2 {
+                
+            storyTextView.text = story5
+            topButton.setTitle("End of Story! Start at the beginning!", for: .normal)
+            bottomButton.isHidden = true
+            storyIndex = 5
+
+        } else if storyIndex == 4 || storyIndex == 5 || storyIndex == 6 && sender.tag == 1 {
+
+            storyIndex = 1
+            bottomButton.isHidden = false
+            startStory()
+            
+        }
                 
         // TODO Step 6: Modify the IF-Statement to complete the story
-        
     
     }
     
+    func startStory() {
+        
+        storyTextView.text = story1
+        topButton.setTitle(answer1a, for: .normal)
+        bottomButton.setTitle(answer1b, for: .normal)
+        
+    }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
